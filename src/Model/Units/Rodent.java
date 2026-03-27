ca016d5e5e0d1d09c7c87386a5aad0b55411fd31
Mxrden <poyapovich22819@gmail.com>
2026-03-27 09:53:40 +0300

    fix: Добавлен конструктор, исправлено поле

--- src/Model/Units/Rodent.java
+++ src/Model/Units/Rodent.java
@@ -1,8 +1,12 @@
-package Model.Units;
+    package Model.Units;
 
-public abstract class Rodent {
-    protected final RodentEffect;
+    public abstract class Rodent {
+        protected final RodentEffect _effect;
 
-    public abstract void onEaten(Snake snake);
+        public abstract void onEaten(Snake snake);
 
-}
+        public Rodent(RodentEffect effect) {
+            this._effect = effect;
+        }
+
+    }
