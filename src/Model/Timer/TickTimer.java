20606baf4e02e8575e9394540563346e967d41d4
Mxrden <poyapovich22819@gmail.com>
2026-03-28 23:04:13 +0300

    feat: добавлен TickTimer

---
+++ src/Model/Timer/TickTimer.java
@@ -0,0 +1,31 @@
+package Model.Timer;
+
+import java.util.Timer;
+
+public class TickTimer {
+    private java.util.Timer executor;
+
+    public void start(long periodMs, Runnable task) {
+        stop();
+
+        executor = new java.util.Timer(true);
+        executor.scheduleAtFixedRate(new java.util.TimerTask() {
+            @Override
+            public void run(){
+                try {
+                    task.run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, periodMs, periodMs);
+    }
+
+
+    public void stop() {
+        if (executor!=null) {
+            executor.cancel();
+            executor=null;
+        }
+    }
+}
